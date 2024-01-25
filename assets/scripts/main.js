04bb2cc9a226b65a857a9fc6f25647a887a6d881
amritregmi26 <amrregmi@gmail.com>
2024-01-25 14:03:22 +0545

    refact: add onclick menu

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,15 +1,18 @@
 let menuBtn = document.querySelector(".menu-btn"),
-    menuList = document.querySelector(".navigation");
+    menuList = document.querySelector(".navigation"),
+    heroSection = document.querySelector(".hero");
 
 
 function toggleMenu() {
     if (menuBtn.innerHTML === "Menu") {
         menuBtn.innerHTML = "Close";
         menuList.style.display = "flex";
+        heroSection.style.marginBlockStart = "9rem";
     }
     else {
         menuBtn.innerHTML = "Menu"
         menuList.style.display = "none";
+        heroSection.style.marginBlockStart = "0rem";
     }
 }
 
